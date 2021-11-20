38a21f9a340e60403d7a1910abc5b56ccfbaa88a
Manuel Bugallo <marvinody@gmail.com>
2021-11-20 18:45:00 -0500

    comments out mercari for now

--- src/extractors/Mercari.ts
+++ src/extractors/Mercari.ts
@@ -1,14 +1,15 @@
-import axios from 'axios'
-import {IExtractor, SearchParams} from './IExtractor'
+// import axios from 'axios'
+// import {IExtractor, SearchParams} from './IExtractor'
+// import * as crypto from "crypto";
 
-const {MERCARI_SEARCH_DPOP} = process.env
+// const {MERCARI_SEARCH_DPOP} = process.env
 
-export default class Mercari implements IExtractor {
-  isEnabled() {
-    return Boolean(MERCARI_SEARCH_DPOP)
-  }
+// export default class Mercari implements IExtractor {
+//   isEnabled() {
+//     return Boolean(MERCARI_SEARCH_DPOP)
+//   }
 
-  search({page, query}: SearchParams) {
+//   search({page, query}: SearchParams) {
 
-  }
-}
+//   }
+// }
